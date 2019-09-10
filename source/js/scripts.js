ee223197d5c32507091b28243dff44debeb13a15
muhammadjewel <msuratov@gmail.com>
2019-09-10 18:23:25 +0500

    Open sitenav when JS is disabled

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -4,17 +4,17 @@ var orderButton = document.querySelector(".featured-product__order");
 var addToCartButtons = document.querySelectorAll(".product__add-to-cart");
 var modal = document.querySelector(".modal");
 
-if (sitenavToggler) {
-  sitenavToggler.addEventListener("click", function() {
-    if (sitenav.classList.contains("sitenav--open")) {
-      sitenav.classList.remove("sitenav--open");
-      sitenav.classList.add("sitenav--closed");
-    } else {
-      sitenav.classList.remove("sitenav--closed");
-      sitenav.classList.add("sitenav--open");
-    }
-  });
-}
+sitenav.classList.remove("sitenav--nojs");
+
+sitenavToggler.addEventListener("click", function() {
+  if (sitenav.classList.contains("sitenav--open")) {
+    sitenav.classList.remove("sitenav--open");
+    sitenav.classList.add("sitenav--closed");
+  } else {
+    sitenav.classList.remove("sitenav--closed");
+    sitenav.classList.add("sitenav--open");
+  }
+});
 
 if (orderButton) {
   orderButton.addEventListener("click", function(evt) {
