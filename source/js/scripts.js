74e22a11e41768036b1b73de52251ac5acd6fe16
muhammadjewel <msuratov@gmail.com>
2019-09-10 16:38:34 +0500

    Add sitenav opening and closing function in mobile screen

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,6 +1,20 @@
+var sitenav = document.querySelector(".sitenav");
+var sitenavToggler = document.querySelector(".sitenav__toggler");
 var orderButton = document.querySelector(".featured-product__order");
 var addToCartButtons = document.querySelectorAll(".product__add-to-cart");
 var modal = document.querySelector(".modal");
+
+if (sitenavToggler) {
+  sitenavToggler.addEventListener("click", function() {
+    if (sitenav.classList.contains("sitenav--open")) {
+      sitenav.classList.remove("sitenav--open");
+      sitenav.classList.add("sitenav--closed");
+    } else {
+      sitenav.classList.remove("sitenav--closed");
+      sitenav.classList.add("sitenav--open");
+    }
+  });
+}
 
 if (orderButton) {
   orderButton.addEventListener("click", function(evt) {
